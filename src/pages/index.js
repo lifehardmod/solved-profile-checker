e79822e960bd79a2c2017e77fc3d25f58f4e5ae3
서주원 <tjwndnjs7@naver.com>
2025-03-15 00:17:04 +0900

    Yesterday 조정

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ function getYesterKSTDate() {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
-  return `${year}-${month}-${day}`;
+  return `${year}-${month}-${day - 1}`;
 }
 function getYesterDay() {
   const date = new Date();
