ab5312bc2b2c4ac22639a9b465bbe07f69a20e35
서주원 <tjwndnjs7@naver.com>
2025-03-15 00:01:13 +0900

    폰트수정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 import "@/styles/globals.css";
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap');
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
